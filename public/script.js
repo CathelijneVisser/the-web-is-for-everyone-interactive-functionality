487b096f64a62e62fa9bfc1834ff036ad474f86c
CathelijneVisser <112855711+CathelijneVisser@users.noreply.github.com>
2023-03-29 13:39:41 +0200

    wip
    form css en beetje js

--- public/script.js
+++ public/script.js
@@ -1,17 +1,35 @@
 const startDate = document.getElementById("dateStart")
 const endDate = document.getElementById("dateEnd")
+const inputs = document.querySelectorAll("input, select")
+const nowButtons = document.querySelectorAll(".book-now-active")
 
 startDate.onchange = () => {
     endDate.value = startDate.value
 }
 
-startDate.value = formatDate();
+startDate.min = formatDate()
+startDate.min = new Date().toISOString().split('T')[0]
 
-startDate.value = new Date().toISOString().split('T')[0];
+nowButtons.forEach(nowButton => {
+  nowButton.addEventListener("click", () => { 
+    window.onload(() => {})
+      startDate.value = formatDate()
+      startDate.value = new Date().toISOString().split('T')[0]
+    }
+  })
+})
+
+inputs.forEach(input => { 
+  input.addEventListener( "invalid", event => { 
+      input.classList.add("error")
+    }, 
+    false 
+  ); 
+});
 
 
-function padTo2Digits(num) {
-  return num.toString().padStart(2, '0');
+function  padTo2Digits(num) {
+  return num.toString().padStart(2, '0')
 }
 
 function formatDate(date = new Date()) {
